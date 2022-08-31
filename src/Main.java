8e0ebb30aa551e38c1b2af4bf71e4d7434331ef7
Gri1023 <hryhorii.bavykin@gmail.com>
2022-08-31 14:53:58 +0300

    added while method

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,36 @@ public class Main {
     public static void main(String[] args) {
         int number = 97;
         boolean prime = false;
+        // method using for
         for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
+           if (number % i == 0) {
                 prime = false;
-                break;
+                 break;
             }
             else
                 prime = true;
         }
         if (prime)
-        System.out.println(number + " is a prime number");
+            System.out.println(number + " is a prime number; used for method");
         else
-        System.out.println(number + " is not a prime number");
+            System.out.println(number + " is not a prime number; used for method");
+        // method using while
+        int n = 2;
+        boolean prime2 = false;
+        while (n<number)
+        {
+            if (number % n == 0) {
+            prime2 = false;
+            n++;
+        }
+            else
+            prime2 = true;
+            break;
+        }
+
+        if (prime2)
+        System.out.println(number + " is a prime number; used while method");
+        else
+        System.out.println(number + " is not a prime number; used while method");
     }
 }
